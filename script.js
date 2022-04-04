ffe5ea23dacd1303fa557ad651418434f7d9a68a
Francesco Moles <francesco.moles@outlook.it>
2022-04-04 16:19:35 +0200

    bonus

--- script.js
+++ script.js
@@ -6,17 +6,12 @@ const app = new Vue({
     },
     mounted() {
         let result;
-        const array = [];
         for (let i = 0; i < this.max; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
                 result = resp.data;
-                array.push(result.response);
                 this.emails.push(result.response);
             });
-        };
-        console.log(array);
-        console.log(array.length);
+        }
     }
+
 });
-
-
